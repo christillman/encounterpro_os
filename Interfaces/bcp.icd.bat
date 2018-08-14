79b824369eece7f255adcd7f50ea0d8e29a70474
Chris Tillman <toff.tillman@gmail.com>
2018-08-15 07:53:56 +1200

    Import ICD10-2019 codes table

--- Interfaces/bcp.icd.bat
+++ Interfaces/bcp.icd.bat
@@ -4,17 +4,20 @@ SET PATH_TO_BCP="C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools
 
 REM The server designation, this one is a local SQL SERVER Express 
 REM I access through Windows user permisson
-SET MSSQLSERVER=DESKTOP-GU15HUD\ENCOUNTERPRO
+SET MSSQLSERVER=DESKTOP-GU15HUD\ENCOUNTERPRO_OS
 
-SET IMPORT_FILE_PATH=E:\EncounterPro\ICD
+SET IMPORT_FILE_PATH=C:\Users\tofft\EncounterPro\ICD
 
-SET SCRIPT_PATH=E:\EncounterPro\encounter_pro_os\Interfaces
+SET SCRIPT_PATH=C:\Users\tofft\EncounterPro\encounterpro_os\Interfaces
 
 REM Execute the table setup script. 
-sqlcmd -i "%SCRIPT_PATH%\create_icd_tables.sql" -S %MSSQLSERVER% -d interfaces -E
+sqlcmd -i "%SCRIPT_PATH%\create_icd_tables.sql" -S %MSSQLSERVER% -d EPRO_OS_105 -E
 
-%PATH_TO_BCP% icd10cm_codes_2018 in "%IMPORT_FILE_PATH%\icd10cm_codes_2018_tabdel.txt" -S %MSSQLSERVER% -d interfaces -T -c
-%PATH_TO_BCP% icd10_gem in "%IMPORT_FILE_PATH%\2018_I10gem.txt" -f "%SCRIPT_PATH%\2018_I10gem.fmt" -S %MSSQLSERVER% -d interfaces -T
-%PATH_TO_BCP% icd9_gem in "%IMPORT_FILE_PATH%\2018_I9gem.txt" -f "%SCRIPT_PATH%\2018_I9gem.fmt" -S %MSSQLSERVER% -d interfaces -T
+%PATH_TO_BCP% icd10cm_codes_2019 in "%IMPORT_FILE_PATH%\icd10cm_codes_2019.txt" -f "%SCRIPT_PATH%\icd10cm_codes.fmt" -S %MSSQLSERVER% -d EPRO_OS_105 -T
 
-%PATH_TO_BCP% icd10pcs_codes_2019 in "%IMPORT_FILE_PATH%\icd10pcs_codes_2019_tabdel.txt" -S %MSSQLSERVER% -d interfaces -T -c
+
+REM %PATH_TO_BCP% icd10cm_codes_2018 in "%IMPORT_FILE_PATH%\icd10cm_codes_2018_tabdel.txt" -S %MSSQLSERVER% -d interfaces -T -c
+REM %PATH_TO_BCP% icd10_gem in "%IMPORT_FILE_PATH%\2018_I10gem.txt" -f "%SCRIPT_PATH%\2018_I10gem.fmt" -S %MSSQLSERVER% -d interfaces -T
+REM %PATH_TO_BCP% icd9_gem in "%IMPORT_FILE_PATH%\2018_I9gem.txt" -f "%SCRIPT_PATH%\2018_I9gem.fmt" -S %MSSQLSERVER% -d interfaces -T
+
+REM %PATH_TO_BCP% icd10pcs_codes_2019 in "%IMPORT_FILE_PATH%\icd10pcs_codes_2019_tabdel.txt" -S %MSSQLSERVER% -d interfaces -T -c
--- Interfaces/create_icd_tables.sql
+++ Interfaces/create_icd_tables.sql
@@ -1,38 +1,10 @@
 
-USE interfaces
-GO
 
-IF EXISTS (SELECT 1 FROM sys.tables WHERE type = 'U' AND name = 'icd10cm_codes_2018')
-	BEGIN DROP TABLE icd10cm_codes_2018 END
-IF EXISTS (SELECT 1 FROM sys.tables WHERE type = 'U' AND name = 'icd10pcs_codes_2019')
-	BEGIN DROP TABLE icd10pcs_codes_2019 END
-GO
-IF EXISTS (SELECT 1 FROM sys.tables WHERE type = 'U' AND name = 'icd10_gem')
-	BEGIN DROP TABLE icd10_gem END
-IF EXISTS (SELECT 1 FROM sys.tables WHERE type = 'U' AND name = 'icd9_gem')
-	BEGIN DROP TABLE icd9_gem END
-GO
+IF EXISTS (SELECT 1 FROM sys.tables WHERE type = 'U' AND name = 'icd10cm_codes_2019')
+	BEGIN DROP TABLE icd10cm_codes_2019 END
 
-CREATE TABLE icd10cm_codes_2018 (
+CREATE TABLE icd10cm_codes_2019 (
 	code varchar(10) NOT NULL,
 	descr varchar(255) NOT NULL
-)
-
-CREATE TABLE icd10pcs_codes_2019 (
-	code varchar(10) NOT NULL,
-	descr varchar(255) NOT NULL
-)
-GO
-
-CREATE TABLE icd10_gem (
-	icd10_code varchar(10) NOT NULL,
-	icd9_code varchar(10) NOT NULL,
-	flags varchar(5)
-)
-
-CREATE TABLE icd9_gem (
-	icd9_code varchar(10) NOT NULL,
-	icd10_code varchar(10) NOT NULL,
-	flags varchar(5)
 )
 GO
