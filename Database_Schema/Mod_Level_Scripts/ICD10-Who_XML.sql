cb586104038547c28e2616f1cfe833c0565730bc
Chris Tillman <toff.tillman@gmail.com>
2019-03-20 20:13:16 +1300

    Move pre-construction doc

--- Database_Schema/Mod_Level_Scripts/ICD10-Who_XML.sql
+++ Database_Schema/Mod_Level_Scripts/ICD10-Who_XML.sql
@@ -14,6 +14,9 @@ descr varchar(500),
 active varchar(1)
 )
 
+GRANT SELECT ON [dbo].[icd10_who_xml] TO CPRSYSTEM
+GRANT SELECT ON [dbo].[icd10_who] TO CPRSYSTEM
+
 INSERT INTO icd10_who_xml(XMLData, LoadedDateTime)
 SELECT CONVERT(XML, BulkColumn) AS BulkColumn, GETDATE() 
 FROM OPENROWSET(BULK 'C:\Users\tofft\EncounterPro\ICD\health_icd10\data\diseases.xml', SINGLE_BLOB) AS x;
@@ -37,20 +40,3 @@ Active [varchar](5) 'field[4]'
 
 EXEC sp_xml_removedocument @hDoc
 GO
-
-select count(*) from  icd10_who r 
-where not exists (select 1 from
- icd10cm_codes w where w.code = r.code)
-
-select count(*) from icd10cm_codes where code != rtrim(code)
- where code like 'F79%' or code like 'Z32%'
-
-SELECT d.icd10_who_code
-FROM c_Assessment_Definition d
-JOIN icd10_who w ON w.code = d.icd10_who_code
-JOIN icd10_rwanda r on r.code = d.icd10_who_code
-WHERE w.active = 'N'
-
-select * from icd10_who
-where len(descr) = 100
-order by code
