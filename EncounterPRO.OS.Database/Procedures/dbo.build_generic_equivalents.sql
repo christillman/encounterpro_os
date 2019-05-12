d7c75b114b4e72cc93b56b8f1d9c5a0617a0b518
Chris Tillman <toff.tillman@gmail.com>
2019-05-12 16:54:34 +1200

    Change boolean column from char(1) to bit

--- EncounterPRO.OS.Database/Procedures/dbo.build_generic_equivalents.sql
+++ EncounterPRO.OS.Database/Procedures/dbo.build_generic_equivalents.sql
@@ -15,7 +15,7 @@ DECLARE build_generic CURSOR FOR
 	FROM c_Drug_Brand_Ingredient bi
 	JOIN c_Drug_Brand b ON b.brand_name_rxcui = bi.brand_name_rxcui
 	JOIN c_Drug_Generic g ON g.generic_rxcui = bi.generic_rxcui_ingredient
-	WHERE b.is_single_ingredient = 'N'
+	WHERE b.is_single_ingredient = 0
 	AND b.generic_rxcui IS NULL
 	ORDER BY bi.brand_name_rxcui, g.generic_name 
 
@@ -33,7 +33,7 @@ WHILE @@FETCH_STATUS = 0
 				FROM c_Drug_Generic g
 				JOIN c_Drug_Brand b ON b.brand_name_rxcui = @last_bn_rxcui
 				WHERE g.generic_name = @generic_equiv
-				AND g.is_single_ingredient = 'N'
+				AND g.is_single_ingredient = 0
 
 				SET @trial_count = @@ROWCOUNT
 
@@ -51,7 +51,7 @@ WHILE @@FETCH_STATUS = 0
 							SELECT @trial_count = count(*) 
 							FROM c_Drug_Generic
 							WHERE generic_name LIKE LEFT(@generic_equiv,@trial_length) + '%'
-							AND is_single_ingredient = 'N'
+							AND is_single_ingredient = 0
 							END
 
 						IF @trial_count = 1
@@ -60,7 +60,7 @@ WHILE @@FETCH_STATUS = 0
 							FROM c_Drug_Generic g
 							JOIN c_Drug_Brand b ON b.brand_name_rxcui = @last_bn_rxcui
 							WHERE g.generic_name LIKE LEFT(@generic_equiv,@trial_length)
-							AND g.is_single_ingredient = 'N'
+							AND g.is_single_ingredient = 0
 
 					END
 
@@ -68,7 +68,7 @@ WHILE @@FETCH_STATUS = 0
 					BEGIN
 						-- Add a new generic; these will have to be cleaned up once in a while
 						INSERT INTO c_Drug_Generic (generic_rxcui, generic_name, is_single_ingredient)
-						VALUES ('G' + @last_bn_rxcui, @generic_equiv, 'N')
+						VALUES ('G' + @last_bn_rxcui, @generic_equiv, 0)
 
 						UPDATE c_Drug_Brand 
 						SET generic_rxcui = 'G' + @last_bn_rxcui
@@ -93,13 +93,13 @@ SET generic_rxcui = g.generic_rxcui
 FROM c_Drug_Generic g
 JOIN c_Drug_Brand b ON b.brand_name_rxcui = @bn_rxcui
 WHERE g.generic_name = @generic_equiv
-AND g.is_single_ingredient = 'N'
+AND g.is_single_ingredient = 0
 
 IF @@ROWCOUNT = 0
 	BEGIN
 		-- Add a new generic
 		INSERT INTO c_Drug_Generic (generic_rxcui, generic_name, is_single_ingredient)
-		VALUES ('G' + @bn_rxcui, @generic_equiv, 'N')
+		VALUES ('G' + @bn_rxcui, @generic_equiv, 0)
 
 		UPDATE c_Drug_Brand 
 		SET generic_rxcui = 'G' + @bn_rxcui
