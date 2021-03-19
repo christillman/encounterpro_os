67321e7d3ebee874ab3cfc6b49ad76134691638e
Chris Tillman <toff.tillman@gmail.com>
2021-03-20 11:45:19 +1300

    Add generic_form_rxcui column for generic list matching

--- Database_Schema/Schema_Objects/Schemas/dbo/Programmability/Stored Procedures/sp_generic_formulations.sql
+++ Database_Schema/Schema_Objects/Schemas/dbo/Programmability/Stored Procedures/sp_generic_formulations.sql
@@ -18,8 +18,8 @@ AS
 
 BEGIN
 
-/* The generic that was picked, if any (0 if unpicked)*/
-SELECT form_descr, form_rxcui, ingr_rxcui, dbo.fn_strength_sort(form_descr) 
+/* The generic that was picked, if any (0 if brand was picked)*/
+SELECT form_descr, form_rxcui, ingr_rxcui, dbo.fn_strength_sort(form_descr) AS strength_sort
 	/* , 'Y' as matches_brand */
 FROM c_Drug_Formulation
 WHERE ingr_rxcui = @ps_generic_ingr_rxcui
@@ -27,15 +27,13 @@ AND valid_in LIKE '%' + @country_code + ';%'
 UNION
 /* If no generic picked, then only the generic formulations matching 
 	the available formulations for the brand that was picked */
-SELECT f.form_descr, f.form_rxcui, f.ingr_rxcui, dbo.fn_strength_sort(f.form_descr)
-FROM c_Drug_Brand b 
-JOIN c_Drug_Formulation f ON f.ingr_rxcui = b.generic_rxcui
-WHERE b.brand_name_rxcui = @ps_brand_name_rxcui
+SELECT f.form_descr, f.form_rxcui, f.ingr_rxcui, dbo.fn_strength_sort(f.form_descr) AS strength_sort
+FROM c_Drug_Formulation fb 
+JOIN c_Drug_Formulation f ON f.form_rxcui = fb.generic_form_rxcui
+WHERE fb.ingr_rxcui = @ps_brand_name_rxcui
 AND f.valid_in LIKE '%' + @country_code + ';%'
+AND fb.valid_in LIKE '%' + @country_code + ';%'
 AND @ps_generic_ingr_rxcui = '0'
-AND EXISTS (SELECT 1 FROM c_Drug_Formulation f2 
-			WHERE f2.ingr_rxcui = b.brand_name_rxcui
-			AND IsNull(dbo.fn_strength(f2.form_descr),'') = IsNull(dbo.fn_strength(f.form_descr),''))
 ORDER BY 4
 
 END
@@ -45,8 +43,7 @@ GRANT EXECUTE
 	ON [dbo].[sp_generic_formulations]
 	TO [cprsystem]
 GO
--- EXEC sp_generic_formulations '0', '1366144', 'ke'
--- EXEC sp_generic_formulations '214186', '0', 'us'
--- EXEC sp_generic_formulations '284635', '0', 'us'
--- EXEC sp_generic_formulations '0', '301543', 'us'
--- exec sp_generic_formulations '0', 'KEB2245', 'KE'
+-- EXEC sp_generic_formulations '0', '1366149', 'ke' -- US only should return no result
+-- EXEC sp_generic_formulations '214186', '0', 'us' -- oddly named PSN
+-- EXEC sp_generic_formulations '284635', '0', 'us' -- slashes in strengths
+-- EXEC sp_generic_formulations '0', '284756', 'KE' -- should only have one in the list
