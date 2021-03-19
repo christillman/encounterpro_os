d0816f88d4765789437b2cba2d4551071b15ef65
Chris Tillman <toff.tillman@gmail.com>
2021-03-20 12:05:55 +1300

    Check for column exists

--- Database_Schema/Mod_Level_Scripts/DDL-211/Tables/20 c_immunization.sql
+++ Database_Schema/Mod_Level_Scripts/DDL-211/Tables/20 c_immunization.sql
@@ -1,7 +1,8 @@
 -- need for country specific immunizations 
 -- identified in Ciru email 26/2/2021
-ALTER TABLE [dbo].[c_Immunization_Schedule]
-ADD valid_in varchar(100)
+if not exists (select * from sys.columns where object_id = object_id('c_Immunization_Schedule') and
+	 name = 'valid_in')
+	ALTER TABLE [dbo].[c_Immunization_Schedule] ADD valid_in varchar(100)
 go
 
 -- set default
