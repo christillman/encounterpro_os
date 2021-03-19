3b730d9fe6dc08c125d7726f05c635784c0d557d
Chris Tillman <toff.tillman@gmail.com>
2021-03-20 10:31:41 +1300

    Don't lose case sensitivity

--- Database_Schema/Mod_Level_Scripts/DDL-211/Tables/30 c_Drug_Tall_Man.sql
+++ Database_Schema/Mod_Level_Scripts/DDL-211/Tables/30 c_Drug_Tall_Man.sql
@@ -1,6 +1,14 @@
 ﻿
+
 ALTER TABLE [c_Drug_Tall_Man]
-ALTER COLUMN spelling varchar(50) NOT NULL
-GO
+DROP CONSTRAINT PK_TallMan 
+
+ALTER TABLE c_Drug_Tall_Man 
+ALTER COLUMN spelling varchar(50) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL
+
 ALTER TABLE [c_Drug_Tall_Man]
 ADD CONSTRAINT PK_TallMan PRIMARY KEY (spelling)
+
+GRANT DELETE, INSERT, SELECT, UPDATE ON c_Drug_Tall_Man TO [cprsystem] AS [dbo]
+GO
+
