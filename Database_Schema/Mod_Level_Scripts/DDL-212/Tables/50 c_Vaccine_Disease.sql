d7c05d0d717359b6b001e0971d52d10f2ba26ca8
Chris Tillman <toff.tillman@gmail.com>
2021-09-12 09:07:52 +1200

    Move update out after ALTER go

--- Database_Schema/Mod_Level_Scripts/DDL-212/Tables/50 c_Vaccine_Disease.sql
+++ Database_Schema/Mod_Level_Scripts/DDL-212/Tables/50 c_Vaccine_Disease.sql
@@ -3,7 +3,7 @@ if not exists (select * from sys.columns where object_id = object_id('c_Vaccine_
 	 name = 'last_updated')
 	BEGIN
 	ALTER TABLE [dbo].[c_Vaccine_Disease] ADD last_updated datetime DEFAULT getdate()
-	UPDATE c_Vaccine_Disease SET last_updated = '2010-01-01' WHERE 1=1
 	END
 go
 
+UPDATE c_Vaccine_Disease SET last_updated = '2010-01-01' WHERE 1=1
