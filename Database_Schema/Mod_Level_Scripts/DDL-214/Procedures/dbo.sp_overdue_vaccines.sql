c387064453e075df98c2fa572712222f1bf52c46
Chris Tillman <toff.tillman@gmail.com>
2023-03-11 14:38:11 +1300

    New and changed procedures for mod 214

---
+++ Database_Schema/Mod_Level_Scripts/DDL-214/Procedures/dbo.sp_overdue_vaccines.sql
@@ -0,0 +1,153 @@
+﻿
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_WARNINGS ON
+SET NOCOUNT ON
+SET XACT_ABORT ON
+GO
+
+-- Drop Procedure [dbo].[sp_overdue_vaccines]
+Print 'Drop Procedure [dbo].[sp_overdue_vaccines]'
+GO
+IF (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'[dbo].[sp_overdue_vaccines]') AND [type]='P'))
+DROP PROCEDURE [dbo].[sp_overdue_vaccines]
+GO
+
+-- Create Procedure [dbo].[sp_overdue_vaccines]
+Print 'Create Procedure [dbo].[sp_overdue_vaccines]'
+GO
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER OFF
+GO
+CREATE   PROCEDURE sp_overdue_vaccines
+AS
+
+SELECT DISTINCT
+	 1
+FROM	c_vaccine_schedule vs WITH (NOLOCK)
+INNER JOIN c_vaccine v WITH (NOLOCK)
+ON	v.vaccine_id = vs.vaccine_id
+WHERE
+	v.description IS NULL
+OR	v.description = ''
+OR	age IS NULL
+OR	age <= 0
+OR	age_unit IS NULL
+OR	age_unit NOT IN ('day', 'd', 'dd' )
+
+IF @@rowcount <> 0
+BEGIN
+	RAISERROR ('Vaccine Schedule is configured incorrectly', 16, 1 )
+
+	RETURN
+END
+
+SELECT 
+	 vs.vaccine_id
+	,COUNT( vs.schedule_sequence ) AS cnt_sequence
+	,MAX( vs.schedule_sequence) as max_sequence
+FROM	c_vaccine_schedule vs WITH (NOLOCK)
+GROUP BY
+	vs.vaccine_id
+HAVING COUNT( vs.schedule_sequence ) <> MAX( vs.schedule_sequence )
+
+IF @@rowcount <> 0
+BEGIN
+	RAISERROR ('Vaccine Sequence must start at 0 and increment by 1', 16, 1 )
+
+	RETURN
+END
+
+DECLARE @tmp TABLE
+(	 cpr_id VARCHAR (24)
+	,date_of_birth DATETIME
+	,vaccine_id VARCHAR (24)
+	,description VARCHAR (80)
+	,vaccine_count INT
+	,next_sequence INT
+	,next_vaccine_due_date DATETIME
+)
+
+-- Get unique list of all applicable vaccines for all active patients under age 18
+
+
+INSERT INTO @tmp
+(	 cpr_id
+	,date_of_birth
+	,vaccine_id
+	,description
+)
+SELECT
+	 p.cpr_id
+	,p.date_of_birth
+	,v.vaccine_id
+	,v.description
+FROM
+	p_patient p WITH (NOLOCK)
+CROSS JOIN c_vaccine_schedule vs WITH (NOLOCK)
+INNER JOIN c_vaccine v WITH (NOLOCK)
+ON	v.vaccine_id = vs.vaccine_id
+WHERE
+	p.patient_status = 'ACTIVE'
+AND	DATEDIFF (year, p.date_of_birth, getdate() ) < 18.0
+AND	v.status = 'OK'
+AND	vs.schedule_sequence = 1
+
+
+-- Count how many times each vaccine has been administered
+
+UPDATE t
+SET	 vaccine_count = (	
+	SELECT COUNT(distinct v.vaccine_id) 
+	FROM p_treatment_item pt WITH (NOLOCK)
+	JOIN c_vaccine v ON pt.drug_id = v.drug_id
+	WHERE t.cpr_id = pt.cpr_id
+	AND	t.vaccine_id = v.vaccine_id
+	AND	pt.treatment_type IN( 'IMMUNIZATION', 'PASTIMMUN' )
+	AND	ISNULL( pt.treatment_status, 'OPEN' ) <> 'CANCELLED'
+)
+FROM @tmp t
+
+--  determine the next time the vaccine is to be administered
+-- If there is no next time, next sequence and date will be NULL
+
+UPDATE t
+SET	 next_sequence = vs.schedule_sequence
+	,next_vaccine_due_date = DATEADD( day, vs.age, t.date_of_birth )
+FROM @tmp t
+INNER JOIN c_vaccine_schedule vs WITH (NOLOCK)
+ON	vs.vaccine_id = t.vaccine_id
+AND	vs.schedule_sequence = t.vaccine_count + 1
+
+-- Get results where there is a "next time" and that "next time is 
+-- <= NOW.	
+
+SELECT
+	 dbo.fn_pretty_name
+		(	last_name
+			,first_name
+			,middle_name
+			,name_suffix
+			,name_prefix
+			,degree 
+		) as patient_name
+	,description
+	,next_sequence
+	,next_vaccine_due_date
+FROM @tmp t
+INNER JOIN p_patient p WITH (NOLOCK)
+ON	p.cpr_id = t.cpr_id
+WHERE
+	next_sequence IS NOT NULL
+AND	next_vaccine_due_date <= getdate()
+ORDER BY
+	 next_vaccine_due_date
+
+
+GO
+GRANT EXECUTE
+	ON [dbo].[sp_overdue_vaccines]
+	TO [cprsystem]
+GO
+
---
+++ Database_Schema/Mod_Level_Scripts/DDL-214/Procedures/dbo.sp_remove_epro_drug.sql
@@ -0,0 +1,298 @@
+
+Print 'Drop Procedure [dbo].[sp_remove_epro_drug]'
+GO
+IF (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'sp_remove_epro_drug') AND [type]='P'))
+DROP PROCEDURE [dbo].[sp_remove_epro_drug]
+GO
+
+Print 'Create Procedure [dbo].[sp_remove_epro_drug]'
+GO
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE sp_remove_epro_drug (
+	@country_code varchar(100),
+	@country_source_id varchar(21),
+	@brand_name_rxcui varchar(20) = NULL,
+	@generic_rxcui varchar(20) = NULL,
+	@brand_form_rxcui varchar(20) = NULL,
+	@generic_form_rxcui varchar(20) = NULL
+	)
+AS
+
+BEGIN
+
+/*
+	@country_code varchar(100), 
+	-- specifying @country_source_id also removes from Kenya_Drugs and c_Drug_Source_Formulation tables
+	@country_source_id varchar(21),
+	-- if only brand is specified, will leave generic in place
+	@brand_name_rxcui varchar(20),
+	-- if generic is used by other countries, it will not be removed
+	@generic_rxcui varchar(20),
+	-- make @brand_name_rxcui and @generic_rxcui NULL if you only want to remove the formulation(s)
+	-- (if either are specified, formulations will also be removed)
+	@brand_form_rxcui varchar(20),
+	@generic_form_rxcui varchar(20)
+*/
+
+DECLARE @msg varchar(100)
+DECLARE @replacement_generic_rxcui varchar(20)
+DECLARE @replacement_brand_name_rxcui varchar(20)
+
+	IF @brand_name_rxcui IS NULL
+		AND @generic_rxcui IS NULL 
+		AND @brand_form_rxcui IS NULL
+		AND @generic_form_rxcui IS NULL
+		BEGIN
+
+		-- remove all brands and generics related to this @country_source_id
+		-- (if not used by other related records)
+		SELECT @brand_name_rxcui = brand_name_rxcui
+		FROM c_Drug_Source_Formulation r
+		WHERE source_id = @country_source_id
+		AND brand_name_rxcui LIKE @country_code + 'BI%'
+		AND country_code = @country_code
+
+		IF @brand_name_rxcui IS NULL
+			SELECT @brand_name_rxcui = brand_name_rxcui
+			FROM c_Drug_Brand b
+			WHERE brand_name_rxcui = @country_code + 'BI' + @country_source_id
+
+		SELECT @replacement_brand_name_rxcui 
+				= upper(@country_code) + 'BI' + min(source_id) 
+			FROM c_Drug_Source_Formulation r2
+			WHERE source_id != @country_source_id
+			AND brand_name_rxcui = @brand_name_rxcui
+			AND country_code = @country_code
+
+		IF @replacement_brand_name_rxcui IS NOT NULL
+			BEGIN
+			-- We are intending to retire this country_drug_id, 
+			-- but it's been chosen as the representative ID for this
+			-- brand name. So we need to substitute the replacement.
+			print 'Replacing ' + @brand_name_rxcui + ' with ' + @replacement_brand_name_rxcui
+			UPDATE c_Drug_Formulation
+			SET ingr_rxcui = @replacement_brand_name_rxcui
+			WHERE ingr_rxcui = @brand_name_rxcui
+			UPDATE c_Drug_Definition
+			SET drug_id = @replacement_brand_name_rxcui
+			WHERE drug_id = @brand_name_rxcui
+			UPDATE c_Drug_Brand
+			SET brand_name_rxcui = @replacement_brand_name_rxcui
+			WHERE brand_name_rxcui = @brand_name_rxcui
+			UPDATE c_Drug_Brand
+			SET drug_id = @replacement_brand_name_rxcui
+			WHERE drug_id = @brand_name_rxcui
+			UPDATE c_Drug_Source_Formulation 
+			SET brand_name_rxcui = @replacement_brand_name_rxcui
+			WHERE source_id != @country_source_id
+			AND brand_name_rxcui = @brand_name_rxcui
+
+			END
+
+		SELECT @generic_rxcui = generic_rxcui
+			FROM c_Drug_Source_Formulation r 
+			WHERE source_id = @country_source_id
+			AND generic_rxcui LIKE @country_code + 'GI%'
+		IF @generic_rxcui IS NULL
+			SELECT @generic_rxcui = generic_rxcui
+			FROM c_Drug_Generic g
+			WHERE generic_rxcui = @country_code + 'GI' + @country_source_id
+		SELECT @replacement_generic_rxcui 
+				= upper(@country_code) + 'GI' + min(source_id) 
+			FROM c_Drug_Source_Formulation r2
+			WHERE source_id != @country_source_id
+			AND generic_rxcui = @generic_rxcui
+			AND country_code = @country_code
+
+		IF @replacement_generic_rxcui IS NOT NULL
+			BEGIN
+			-- We are intending to retire this country_drug_id, 
+			-- but it's been chosen as the representative ID for this
+			-- generic. So we need to substitute the replacement.
+			print 'Replacing ' + @generic_rxcui + ' with ' + @replacement_generic_rxcui
+			UPDATE c_Drug_Formulation
+			SET ingr_rxcui = @replacement_generic_rxcui
+			WHERE ingr_rxcui = @generic_rxcui
+			UPDATE c_Drug_Brand
+			SET generic_rxcui = @replacement_generic_rxcui
+			WHERE generic_rxcui = @generic_rxcui
+			UPDATE c_Drug_Definition
+			SET drug_id = @replacement_generic_rxcui
+			WHERE drug_id = @generic_rxcui
+			UPDATE c_Drug_Generic
+			SET generic_rxcui = @replacement_generic_rxcui
+			WHERE generic_rxcui = @generic_rxcui
+			UPDATE c_Drug_Generic
+			SET drug_id = @replacement_generic_rxcui
+			WHERE drug_id = @generic_rxcui
+			UPDATE c_Drug_Source_Formulation 
+			SET generic_rxcui = @replacement_generic_rxcui
+			WHERE source_id != @country_source_id
+			AND generic_rxcui = @generic_rxcui
+
+			END
+
+		END
+
+	IF @brand_name_rxcui IS NOT NULL
+		BEGIN
+		-- remove brands with their formulations and packages
+			SELECT form_rxcui
+			INTO #remove_brand_forms
+			FROM c_Drug_Formulation
+			WHERE form_rxcui LIKE @country_code + 'B%'
+				AND form_rxcui = replace(@brand_name_rxcui,
+				@country_code + 'BI', @country_code + 'B')
+			UNION
+			SELECT form_rxcui
+			FROM c_Drug_Formulation
+			WHERE form_rxcui LIKE @country_code + 'B%'
+				AND ingr_rxcui = @brand_name_rxcui
+
+			SELECT @msg = 'Removing ' + convert(varchar(2), count(*)) 
+				+ ' brand formulations for ' 
+				+ replace(@brand_name_rxcui, @country_code + 'BI', @country_code + 'B')
+			FROM #remove_brand_forms
+			print @msg
+
+			SELECT package_id
+			INTO #remove_brand_pkgs
+			FROM c_Drug_Package
+			WHERE form_rxcui IN (SELECT form_rxcui FROM #remove_brand_forms)
+			DELETE FROM c_Package_Administration_Method
+			WHERE package_id IN (SELECT package_id FROM #remove_brand_pkgs)
+			DELETE FROM c_Package
+			WHERE package_id IN (SELECT package_id FROM #remove_brand_pkgs)
+			DELETE FROM c_Drug_Package
+			WHERE package_id IN (SELECT package_id FROM #remove_brand_pkgs)
+			DELETE FROM c_Drug_Formulation
+			WHERE form_rxcui IN (SELECT form_rxcui FROM #remove_brand_forms)
+
+			SELECT @msg = 'Removing drug defn ' + drug_id + ', ' + common_name 
+			FROM c_Drug_Definition
+			WHERE drug_id = (
+				SELECT drug_id FROM c_Drug_Brand
+				WHERE brand_name_rxcui = @brand_name_rxcui
+				AND @brand_name_rxcui LIKE @country_code + 'BI%'
+				)
+			IF @@rowcount = 0
+				SET @msg = 'c_Drug_Definition ' + @brand_name_rxcui + ' not found'
+			print @msg
+			DELETE FROM c_Drug_Definition
+			WHERE drug_id = (
+				SELECT drug_id FROM c_Drug_Brand
+				WHERE brand_name_rxcui = @brand_name_rxcui
+				AND @brand_name_rxcui LIKE @country_code + 'BI%'
+				)
+
+			SELECT @msg = 'Removing brand ' + brand_name_rxcui + ', ' + brand_name 
+			FROM c_Drug_Brand
+			WHERE brand_name_rxcui = @brand_name_rxcui
+				AND @brand_name_rxcui LIKE @country_code + 'BI%'
+			IF @@rowcount = 0
+				SET @msg = 'c_Drug_Brand ' + @brand_name_rxcui + ' not found'
+			print @msg
+			DELETE FROM c_Drug_Brand
+			WHERE brand_name_rxcui = @brand_name_rxcui
+				AND @brand_name_rxcui LIKE @country_code + 'BI%'
+
+		END
+
+	IF @generic_rxcui IS NOT NULL
+		BEGIN
+		-- remove generics with their formulations and packages
+
+			SELECT form_rxcui
+			INTO #remove_generic_forms
+			FROM c_Drug_Formulation
+			WHERE form_rxcui LIKE @country_code + 'G%'
+				AND form_rxcui = replace(@generic_rxcui,
+				@country_code + 'GI', @country_code + 'G')
+			UNION
+			SELECT form_rxcui
+			FROM c_Drug_Formulation
+			WHERE form_rxcui LIKE @country_code + 'G%'
+				AND ingr_rxcui = @generic_rxcui
+			SELECT @msg = 'Removing ' + convert(varchar(2), count(*)) 
+				+ ' generic formulations for ' + @generic_rxcui
+			FROM #remove_generic_forms
+			print @msg
+
+			SELECT package_id
+			INTO #remove_generic_pkgs
+			FROM c_Drug_Package
+			WHERE form_rxcui IN (SELECT form_rxcui FROM #remove_generic_forms)
+			DELETE FROM c_Package_Administration_Method
+			WHERE package_id IN (SELECT package_id FROM #remove_generic_pkgs)
+			DELETE FROM c_Package
+			WHERE package_id IN (SELECT package_id FROM #remove_generic_pkgs)
+			DELETE FROM c_Drug_Package
+			WHERE package_id IN (SELECT package_id FROM #remove_generic_pkgs)
+			DELETE FROM c_Drug_Formulation
+			WHERE form_rxcui IN (SELECT form_rxcui FROM #remove_generic_forms)
+
+			SELECT @msg = 'Removing drug defn ' + drug_id + ', ' + generic_name 
+			FROM c_Drug_Definition
+			WHERE drug_id = (
+				SELECT drug_id FROM c_Drug_Generic
+				WHERE generic_rxcui = @generic_rxcui
+				AND @generic_rxcui LIKE @country_code + 'GI%'
+				)
+			IF @@rowcount = 0
+				SET @msg = 'c_Drug_Definition ' + @generic_rxcui + ' not found'
+			print @msg
+			DELETE FROM c_Drug_Definition
+			WHERE drug_id = (
+				SELECT drug_id FROM c_Drug_Generic
+				WHERE generic_rxcui = @generic_rxcui
+				AND @generic_rxcui LIKE @country_code + 'GI%'
+				)
+
+			SELECT @msg = 'Removing generic ' + generic_rxcui + ', ' + generic_name 
+			FROM c_Drug_Generic 
+			WHERE generic_rxcui = @generic_rxcui
+				AND @generic_rxcui LIKE @country_code + 'GI%'
+			IF @@rowcount = 0
+				SET @msg = 'c_Drug_Generic ' + @generic_rxcui + ' not found'
+			print @msg
+			DELETE FROM c_Drug_Generic
+			WHERE generic_rxcui = @generic_rxcui
+				AND @generic_rxcui LIKE @country_code + 'GI%'
+
+		END
+
+		
+	IF @generic_form_rxcui IS NOT NULL OR @brand_form_rxcui IS NOT NULL
+		BEGIN
+		-- remove the specified formulations with their packages
+		SELECT package_id
+		INTO #remove_pkgs
+		FROM c_Drug_Package
+		WHERE form_rxcui IN (@brand_form_rxcui, @generic_form_rxcui)
+		DELETE FROM c_Package_Administration_Method
+		WHERE package_id IN (SELECT package_id FROM #remove_pkgs)
+		DELETE FROM c_Package
+		WHERE package_id IN (SELECT package_id FROM #remove_pkgs)
+		DELETE FROM c_Drug_Package
+		WHERE package_id IN (SELECT package_id FROM #remove_pkgs)
+		DELETE FROM c_Drug_Formulation
+		WHERE form_rxcui IN (@brand_form_rxcui, @generic_form_rxcui)
+		END
+
+	IF @country_source_id IS NOT NULL
+		BEGIN
+
+		DELETE FROM c_Drug_Source_Formulation
+		WHERE source_id = @country_source_id
+		AND country_code = @country_code
+		DELETE FROM Kenya_Drugs
+		WHERE Retention_No = @country_source_id
+		AND @country_code = 'ke'
+		DELETE FROM Uganda_Drugs
+		WHERE NDA_MAL_HDP = @country_source_id
+		AND @country_code = 'ug'
+
+		END
+END
