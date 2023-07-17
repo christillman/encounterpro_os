5b2c44f7973a3cb0079a92df4231df634a6d9242
Chris Tillman <toff.tillman@gmail.com>
2023-07-18 11:37:49 +1200

    Need to drop column and constraint before setting generic_name not null

--- Database_Schema/Mod_Level_Scripts/DDL-217/Tables/Add_PK_Drug_Tables.sql
+++ Database_Schema/Mod_Level_Scripts/DDL-217/Tables/Add_PK_Drug_Tables.sql
@@ -1,3 +1,10 @@
+
+
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+GO
+
 -- Would prevent constraint being created
 delete from c_Drug_Generic
 where generic_rxcui = 'UGGI6489'
@@ -7,11 +14,26 @@ and generic_name = 'bromhexin / uaifenesi / erbutaline'
 UPDATE c_Drug_Generic SET drug_id = 'RXNG' + generic_rxcui
 WHERE generic_rxcui IN ('10869','898404')
 
+GO
+-- Need to drop column and constraint before setting generic_name not null
+if exists (select * from sys.columns where object_id = object_id('c_Drug_Generic') and
+	 name = 'uq_name_checksum')
+	BEGIN
+	ALTER TABLE c_Drug_Generic DROP CONSTRAINT IF EXISTS uq_generic_name 
+	ALTER TABLE c_Drug_Generic DROP COLUMN uq_name_checksum
+	END
+
 ALTER TABLE c_Drug_Generic
 ALTER COLUMN generic_rxcui varchar(30) NOT NULL
 
 ALTER TABLE c_Drug_Generic
 ALTER COLUMN generic_name varchar(2000) NOT NULL
+
+GO
+ALTER TABLE c_Drug_Generic ADD uq_name_checksum AS (CHECKSUM(generic_name)
+	) PERSISTED,	
+	CONSTRAINT uq_generic_name UNIQUE (uq_name_checksum)
+GO
 
 IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'pk_Drug_Generic') AND type = 'K')
 ALTER TABLE c_Drug_Generic
@@ -19,12 +41,18 @@ ADD CONSTRAINT pk_Drug_Generic PRIMARY KEY  (generic_rxcui)
 
 DROP INDEX IF EXISTS c_Drug_Generic.uq_generic_drug_id
 CREATE UNIQUE INDEX uq_generic_drug_id ON c_Drug_Generic (drug_id)
+GO
 
+DROP INDEX IF EXISTS c_Drug_Brand.uq_brand_name
+GO
 ALTER TABLE c_Drug_Brand
 ALTER COLUMN brand_name_rxcui varchar(30) NOT NULL
 
+delete from c_Drug_Brand where brand_name is null
+
 ALTER TABLE c_Drug_Brand
 ALTER COLUMN brand_name varchar(200) NOT NULL
+GO
 
 IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'pk_Drug_Brand') AND type = 'K')
 ALTER TABLE c_Drug_Brand
