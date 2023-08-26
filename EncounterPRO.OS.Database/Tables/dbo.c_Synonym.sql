1c22d0b73668f614bcaafdf7d94a661ae833aa97
Chris Tillman <toff.tillman@gmail.com>
2023-08-26 17:44:24 +1200

    c_Synonym

---
+++ EncounterPRO.OS.Database/Tables/dbo.c_Synonym.sql
@@ -0,0 +1,19 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[c_Synonym]') AND type in (N'U'))
+DROP TABLE [c_Synonym]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [c_Synonym](
+	[term] [varchar](250) NOT NULL,
+	[term_type] [varchar](30) NOT NULL,
+	[alternate] [varchar](250) NOT NULL,
+ CONSTRAINT [PK_Synonym] PRIMARY KEY CLUSTERED 
+(
+	[term_type] ASC,
+	[term] ASC,
+	[alternate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
