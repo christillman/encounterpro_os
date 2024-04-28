dd44ad712eadd2c9d4043312d6b8092ca6961c15
Chris Tillman <toff.tillman@gmail.com>
2024-04-28 15:20:47 +1200

    Because universal_flag is inoperative (no Epro_Updates), and the results are the same for either 'N' or 'C', simplify the logic

--- EncounterPRO.OS.Database/Functions/dbo.fn_get_global_preference.sql
+++ EncounterPRO.OS.Database/Functions/dbo.fn_get_global_preference.sql
@@ -1,48 +1,4 @@
-﻿--EncounterPRO Open Source Project
---
---Copyright 2010-2011 The EncounterPRO Foundation, Inc.
---
---This program is free software: you can redistribute it and/or modify it under the terms of 
---the GNU Affero General Public License as published by the Free Software Foundation, either 
---version 3 of the License, or (at your option) any later version.
---
---This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
---without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
---See the GNU Affero General Public License for more details.
---
---You should have received a copy of the GNU Affero General Public License along with this 
---program. If not, see http://www.gnu.org/licenses.
---
---EncounterPRO Open Source Project (“The Project”) is distributed under the GNU Affero 
---General Public License version 3, or any later version. As such, linking the Project 
---statically or dynamically with other components is making a combined work based on the 
---Project. Thus, the terms and conditions of the GNU Affero General Public License version 3, 
---or any later version, cover the whole combination.
---
---However, as an additional permission, the copyright holders of EncounterPRO Open Source 
---Project give you permission to link the Project with independent components, regardless of 
---the license terms of these independent components, provided that all of the following are true:
---
---1. All access from the independent component to persisted data which resides
---   inside any EncounterPRO Open Source data store (e.g. SQL Server database) 
---   be made through a publically available database driver (e.g. ODBC, SQL 
---   Native Client, etc) or through a service which itself is part of The Project.
---2. The independent component does not create or rely on any code or data 
---   structures within the EncounterPRO Open Source data store unless such 
---   code or data structures, and all code and data structures referred to 
---   by such code or data structures, are themselves part of The Project.
---3. The independent component either a) runs locally on the user's computer,
---   or b) is linked to at runtime by The Project’s Component Manager object 
---   which in turn is called by code which itself is part of The Project.
---
---An independent component is a component which is not derived from or based on the Project.
---If you modify the Project, you may extend this additional permission to your version of 
---the Project, but you are not obligated to do so. If you do not wish to do so, delete this 
---additional permission statement from your version.
---
------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------
-
+﻿
 SET ARITHABORT ON
 SET NUMERIC_ROUNDABORT OFF
 SET CONCAT_NULL_YIELDS_NULL ON
@@ -74,14 +30,36 @@ RETURNS varchar(255)
 AS
 
 BEGIN
+/* 7.2.1.9: eliminate unused call to c_Database_Status */
 
 DECLARE @ls_preference_value varchar(255),
 		@ls_universal_flag char(1),
-		@ll_customer_id int,
 		@ll_error int,
 		@ll_rowcount int
 
-SELECT @ls_preference_value = NULL
+SET @ls_preference_value = NULL
+
+/* Because universal_flag is inoperative (no Epro_Updates),
+	and the results are the same for either 'N' or 'C',
+	simplify the logic 
+	*/
+
+SELECT @ls_preference_value = preference_value
+FROM o_preferences
+WHERE preference_type = @ps_preference_type
+AND preference_level = 'Global'
+AND preference_key = 'Global'
+AND preference_id = @ps_preference_id
+
+/*
+
+SELECT @ll_rowcount = @@ROWCOUNT,
+		@ll_error = @@ERROR
+		
+IF @ll_error <> 0
+	RETURN @ls_preference_value
+END
+
 
 SELECT @ls_universal_flag = universal_flag
 FROM c_Preference
@@ -96,9 +74,6 @@ IF @ll_error <> 0
 IF @ll_rowcount = 0
 	SET @ls_universal_flag = 'N'
 
-SELECT @ll_customer_id = customer_id
-FROM c_Database_Status
-
 SELECT @ll_rowcount = @@ROWCOUNT,
 		@ll_error = @@ERROR
 		
@@ -107,6 +82,7 @@ IF @ll_error <> 0
 
 IF @ll_rowcount = 0
 	RETURN @ls_preference_value
+
 
 
 IF @ls_universal_flag = 'N'
@@ -140,7 +116,9 @@ IF @ls_universal_flag = 'C'
 	IF @ll_error <> 0
 		RETURN @ls_preference_value
 	END
-
+*/
+/* This always returns null due to missing Epro_Updates (fn_get_universal_preference) */
+/*
 IF @ls_universal_flag = 'Y' OR (@ls_universal_flag = 'C' AND @ls_preference_value IS NULL)
 	BEGIN
 	SET @ls_preference_value = dbo.fn_get_universal_preference(@ps_preference_id)
@@ -157,6 +135,7 @@ IF @ls_universal_flag = 'Y' OR (@ls_universal_flag = 'C' AND @ls_preference_valu
 		
 		END
 	END
+*/
 
 RETURN @ls_preference_value
 
@@ -164,8 +143,6 @@ END
 
 
 GO
-GRANT EXECUTE
-	ON [dbo].[fn_get_global_preference]
-	TO [cprsystem]
+GRANT EXECUTE ON [dbo].[fn_get_global_preference] TO [cprsystem]
 GO
 
