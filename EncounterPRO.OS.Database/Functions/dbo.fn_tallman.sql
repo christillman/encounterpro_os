dd7dfe06844ee1ecc1a51fc67cde19364e5ffcae
Chris Tillman <toff.tillman@gmail.com>
2024-10-31 20:32:32 +1300

    Rethink tallman functions for efficiency and correctness.

--- EncounterPRO.OS.Database/Functions/dbo.fn_tallman.sql
+++ EncounterPRO.OS.Database/Functions/dbo.fn_tallman.sql
@@ -10,37 +10,54 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE FUNCTION dbo.fn_tallman (@drug_description varchar(1000))
-RETURNS varchar(1000) 
+RETURNS varchar(2000) 
 AS BEGIN
-	DECLARE @tallman_version varchar(1000)
-	IF dbo.fn_needs_tallman(@drug_description) = 0
-		RETURN @drug_description
+	DECLARE @tallman_version varchar(2000) = @drug_description
+	DECLARE @limit int = 15	
+	DECLARE @spelling varchar(200)
 
-	SELECT @tallman_version = REPLACE(@drug_description COLLATE SQL_Latin1_General_CP1_CI_AS, tm.spelling, tm.spelling)
-	FROM c_Drug_Tall_Man tm
-	WHERE (@drug_description COLLATE SQL_Latin1_General_CP1_CI_AS LIKE tm.spelling + '%'
-			and @drug_description COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE tm.spelling + '%')
-		or (@drug_description COLLATE SQL_Latin1_General_CP1_CI_AS LIKE '% ' + tm.spelling + '%'
-			and @drug_description COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE '% ' + tm.spelling + '%')
+	DECLARE cr_update CURSOR FOR 
+	SELECT spelling from c_Drug_Tall_Man 
+	WHERE @tallman_version LIKE '%' + spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '%'
+	
+	IF @@ROWCOUNT = 0 RETURN @drug_description
 
-	/* May need more than one replacement! */
-	WHILE dbo.fn_needs_tallman(@tallman_version) = 1
-		SELECT @tallman_version = REPLACE(@tallman_version COLLATE SQL_Latin1_General_CP1_CI_AS, tm.spelling, tm.spelling)
-		FROM c_Drug_Tall_Man tm
-		WHERE (@tallman_version COLLATE SQL_Latin1_General_CP1_CI_AS LIKE tm.spelling + '%'
-				and @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE tm.spelling + '%')
-			or (@tallman_version COLLATE SQL_Latin1_General_CP1_CI_AS LIKE '% ' + tm.spelling + '%'
-				and @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE '% ' + tm.spelling + '%')
-		
+	OPEN cr_update
+
+	FETCH NEXT FROM cr_update INTO @spelling
+ 
+	WHILE @@FETCH_STATUS = 0
+		BEGIN
+		SELECT @tallman_version = REPLACE(@tallman_version, @spelling, @spelling)
+		FROM c_1_record
+		WHERE (@tallman_version LIKE @spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '%'
+				AND @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE @spelling + '%')
+			OR (@tallman_version LIKE '% ' + @spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '%'
+				AND @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE '% ' + @spelling + '%')
+			OR (@tallman_version LIKE '%(' + @spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '%'
+				AND @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS NOT LIKE '%(' + @spelling + '%')
+			OR (@tallman_version = @spelling COLLATE SQL_Latin1_General_CP1_CI_AS
+				AND @tallman_version COLLATE SQL_Latin1_General_CP1_CS_AS != @spelling)
+			AND @tallman_version NOT LIKE '%[A-Za-z]' + @spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '%'
+			AND @tallman_version NOT LIKE '%' + @spelling COLLATE SQL_Latin1_General_CP1_CI_AS + '[A-Za-z]%'
+
+		FETCH NEXT FROM cr_update INTO @spelling
+		END
+
+	CLOSE cr_update
+	DEALLOCATE cr_update
+	
 	RETURN @tallman_version
 END
 
 go
+
 /* test
-select descr, dbo.fn_tallman(descr) as sort_expression
-from c_Drug_Formulation
-where descr like 'actonel%' or descr like 'bleph%' 
-order by dbo.fn_tallman(descr)
+
+select dbo.fn_tallman('acetaminophen / dextromethorphan / pseudoephedrine')
+select dbo.fn_tallman('norvasc (Amlodipine)')
+select dbo.fn_tallman('lactose')
+
 */
 
 GRANT EXECUTE ON dbo.fn_tallman TO cprsystem
