a6dcb7f0185c976b19be167c953c50fe02041d6b
Chris Tillman <toff.tillman@gmail.com>
2024-12-14 11:53:39 +1300

    Add missing permissions

---
+++ Database_Schema/Mod_Level_Scripts/DDL-229/Tables/Perms.sql
@@ -0,0 +1,131 @@
+
+GRANT DELETE ON [dbo].[p_patient_list_item] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[p_patient_list_item] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[p_patient_list_item] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[p_patient_list_item] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[p_patient_list_item] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Pack_Formulation] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Pack_Formulation] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Pack_Formulation] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Pack_Formulation] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Pack_Formulation] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Tall_Man] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Tall_Man] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Tall_Man] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Tall_Man] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Tall_Man] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_EPC] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_EPC] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_EPC] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_EPC] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_EPC] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_EPC] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_EPC] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_EPC] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_EPC] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_EPC] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Package_Administration_Method] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Package_Administration_Method] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Package_Administration_Method] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Package_Administration_Method] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Package_Administration_Method] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[p_Observation_Comment_Save] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[p_Observation_Comment_Save] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[p_Observation_Comment_Save] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[p_Observation_Comment_Save] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[p_Observation_Comment_Save] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Formulation] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Formulation] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Formulation] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Formulation] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Formulation] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Pack] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Pack] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Pack] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Pack] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Pack] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Source_Formulation] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Source_Formulation] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Source_Formulation] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Source_Formulation] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Source_Formulation] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Drug_Generic] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Drug_Generic] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Drug_Generic] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Drug_Generic] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Drug_Generic] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[c_Synonym] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[c_Synonym] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[c_Synonym] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[c_Synonym] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[c_Synonym] TO [cprsystem]
+GO
+GRANT DELETE ON [dbo].[o_User_Logins] TO [cprsystem]
+GO
+GRANT INSERT ON [dbo].[o_User_Logins] TO [cprsystem]
+GO
+GRANT REFERENCES ON [dbo].[o_User_Logins] TO [cprsystem]
+GO
+GRANT SELECT ON [dbo].[o_User_Logins] TO [cprsystem]
+GO
+GRANT UPDATE ON [dbo].[o_User_Logins] TO [cprsystem]
+GO
