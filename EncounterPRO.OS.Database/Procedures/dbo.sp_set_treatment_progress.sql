a0d91aa26484a63d6cf4d905ea7fc3c5cb00ced7
Chris Tillman <toff.tillman@gmail.com>
2025-03-22 14:07:34 +1300

    Add debug logging for #87

--- EncounterPRO.OS.Database/Procedures/dbo.sp_set_treatment_progress.sql
+++ EncounterPRO.OS.Database/Procedures/dbo.sp_set_treatment_progress.sql
@@ -1,48 +1,4 @@
-﻿--EncounterPRO Open Source Project
---
---Copyright 2010-2011 The EncounterPRO Foundation, Inc.
---
---This program is free software: you can redistribute it and/or modify it under the terms of 
---the GNU Affero General Public License as published by the Free Software Foundation, either 
---version 3 of the License, or (at your option) any later version.
---
---This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
---without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
---See the GNU Affero General Public License for more details.
---
---You should have received a copy of the GNU Affero General Public License along with this 
---program. If not, see http://www.gnu.org/licenses.
---
---EncounterPRO Open Source Project (“The Project”) is distributed under the GNU Affero 
---General Public License version 3, or any later version. As such, linking the Project 
---statically or dynamically with other components is making a combined work based on the 
---Project. Thus, the terms and conditions of the GNU Affero General Public License version 3, 
---or any later version, cover the whole combination.
---
---However, as an additional permission, the copyright holders of EncounterPRO Open Source 
---Project give you permission to link the Project with independent components, regardless of 
---the license terms of these independent components, provided that all of the following are true:
---
---1. All access from the independent component to persisted data which resides
---   inside any EncounterPRO Open Source data store (e.g. SQL Server database) 
---   be made through a publically available database driver (e.g. ODBC, SQL 
---   Native Client, etc) or through a service which itself is part of The Project.
---2. The independent component does not create or rely on any code or data 
---   structures within the EncounterPRO Open Source data store unless such 
---   code or data structures, and all code and data structures referred to 
---   by such code or data structures, are themselves part of The Project.
---3. The independent component either a) runs locally on the user's computer,
---   or b) is linked to at runtime by The Project’s Component Manager object 
---   which in turn is called by code which itself is part of The Project.
---
---An independent component is a component which is not derived from or based on the Project.
---If you modify the Project, you may extend this additional permission to your version of 
---the Project, but you are not obligated to do so. If you do not wish to do so, delete this 
---additional permission statement from your version.
---
------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------
-
+﻿
 SET ARITHABORT ON
 SET NUMERIC_ROUNDABORT OFF
 SET CONCAT_NULL_YIELDS_NULL ON
@@ -84,7 +40,8 @@ DECLARE @ll_encounter_log_id integer,
 	@ll_patient_workplan_id int,
 	@ls_progress_value varchar(40),
 	@ll_length int,
-	@ll_patient_workplan_item_id int
+	@ll_patient_workplan_item_id int,
+	@ls_message varchar(1000)
 
 
 -- mark 11/25/03 Added fix for problem of missing (cancelled) PFSH
@@ -138,6 +95,22 @@ IF @ps_progress_type = 'Refill'
 				AND t.treatment_status = 'MODIFIED'
 				AND t.end_date < @pdt_progress_date_time)
 		SET @pl_treatment_id = dbo.fn_get_current_treatment(@ps_cpr_id, @pl_treatment_id)
+
+SET @ls_message = 'Inserting treatment progress: ' 
+	+ coalesce(@ps_cpr_id,'null') + ', '
+	+ coalesce(convert(varchar(12), @pl_treatment_id),'null') + ', '
+	+ coalesce(convert(varchar(12), @pl_encounter_id),'null') + ', '
+	+ coalesce(@ps_progress_type,'null') + ', '
+	+ coalesce(@ps_progress_key,'null') + ', '
+	+ coalesce(@ps_progress,'null') + ', '
+	+ coalesce(convert(varchar(20), @pdt_progress_date_time),'null') + ', '
+	+ coalesce(convert(varchar(12), @pl_patient_workplan_item_id),'null') + ', '
+	+ coalesce(convert(varchar(12), @pl_risk_level),'null') + ', '
+	+ coalesce(convert(varchar(12), @pl_attachment_id),'null') + ', '
+	+ coalesce(@ps_user_id,'null') + ', '
+	+ coalesce(@ps_created_by,'null') + '. '
+
+exec jmj_new_log_message 'DEBUG', 'sp_set_treatment_progress', '', @ls_message
 
 -- First add the progress record.  If the length of @ps_progress is <= 40 then
 -- store the value in [progress_value].  Otherwise store it in [progress].
